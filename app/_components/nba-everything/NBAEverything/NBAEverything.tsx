b223851cd8b381eaf3e08ce83153f68608598495
Derek Drill <derekrdrill@gmail.com>
2025-02-23 16:37:45 -0500

    🧹 removes clg

--- app/_components/nba-everything/NBAEverything/NBAEverything.tsx
+++ app/_components/nba-everything/NBAEverything/NBAEverything.tsx
@@ -12,38 +12,6 @@ import {
 import { useNBAEverythingStore } from '@store';
 import { NBATeamStats, NBATeam } from '@types';
 
-// const getTeamSeasonData = async ({ season, teamId }: { season?: number; teamId?: number }) => {
-//   const seasonAveragesOptions = {
-//     method: 'GET',
-//     url: `${process.env.NEXT_PUBLIC_NBA_EVERYTHING_API_URL}/games/${teamId}/${season}`,
-//   };
-
-//   try {
-//     const response = await axios.request(seasonAveragesOptions);
-
-//     return response.data.data || [];
-//   } catch (error) {
-//     console.error(error);
-//     throw new Error('Failed to fetch current teams');
-//   }
-// };
-
-// const getCurrentTeams = async (): Promise<NBATeam[]> => {
-//   const teamOptions = {
-//     method: 'GET',
-//     url: `${process.env.NEXT_PUBLIC_NBA_EVERYTHING_API_URL}/teams/current`,
-//   };
-
-//   try {
-//     const response = await axios.request(teamOptions);
-
-//     return response.data.data;
-//   } catch (error) {
-//     console.error(error);
-//     throw new Error('Failed to fetch current teams');
-//   }
-// };
-
 export default function NBAEverything() {
   const { selectedTeam, selectedSeason } = useNBAEverythingStore();
 

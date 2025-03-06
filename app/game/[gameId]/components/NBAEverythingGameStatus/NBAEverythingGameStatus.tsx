0adf48f49130757f078cc73f2853087d6f7a469c
Derek Drill <derekdrillwebdev@gmail.com>
2025-03-05 21:01:48 -0500

    💄 updates mobile styling for game view (#23)

--- app/game/[gameId]/components/NBAEverythingGameStatus/NBAEverythingGameStatus.tsx
+++ app/game/[gameId]/components/NBAEverythingGameStatus/NBAEverythingGameStatus.tsx
@@ -1,9 +1,14 @@
+import classNames from 'classnames';
 import { ArrowLeftIcon, ArrowRightIcon } from '@node_modules/@heroicons/react/20/solid';
 
 import { useNBAEverythingStore } from '@store';
 import { isHomeWin, isVisitorWin } from '@app/game/[gameId]/helpers';
 
-export default function NBAEverythingGameStatus() {
+type NBAEverythingGameStatusProps = {
+  styles?: string;
+};
+
+export default function NBAEverythingGameStatus({ styles }: NBAEverythingGameStatusProps) {
   const { selectedGame } = useNBAEverythingStore();
 
   const homeTeamScore = selectedGame?.home_team_score;
@@ -13,12 +18,12 @@ export default function NBAEverythingGameStatus() {
   const isFinal = selectedGame?.time === 'Final';
 
   return (
-    <div className='flex gap-3'>
-      <div className='h-6 w-6'>
+    <div className={classNames('flex gap-3 justify-end md:justify-start', styles)}>
+      <div className='hidden h-6 w-6 md:block'>
         {isVisitorWin2 && isFinal && <ArrowLeftIcon className='h-full w-full' />}
       </div>
       <p className='font-bold text-lg'>{selectedGame?.time}</p>
-      <div className='h-6 w-6'>
+      <div className='hidden h-6 w-6 md:block'>
         {isHomeWin2 && isFinal && <ArrowRightIcon className='h-full w-full' />}
       </div>
     </div>
